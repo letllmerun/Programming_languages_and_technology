d778f8b4c07fe57530f6586cf32478bdc13b00ba
Leila <ttooxxaaeell@gmail.com>
2026-09-16 14:22:00 +0500

    task1.py

---
+++ Lab3/task1.py
@@ -0,0 +1,11 @@
+def area(self):
+    return self.width * self.height
+
+def perimeter(self):
+    return 2 * (self.width + self.height)
+
+def show_info(self):
+    print("Width:", self.width)
+    print("Height:", self.height)
+    print("Area:", self.area())
+    print("Perimeter:", self.perimeter())
